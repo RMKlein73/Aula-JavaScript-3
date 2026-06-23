bd460007e298051387839a9e7031066ca9a0ce38
RMKlein73 <heyrafiz.1@gmail.com>
2026-06-22 21:04:38 -0300

    Add files via upload

---
+++ votar.js
@@ -0,0 +1,16 @@
+let pessoa1 = "Rafael"
+let idade1 = 16
+let voto1 = (idade1 >= 16) 
+
+? "Pode votar!" 
+: "Não pode votar..."
+
+let pessoa2 = "Luisa"
+let idade2 = 14
+let voto2 = (idade2 >= 16) 
+
+? "Pode votar!" 
+: "Não pode votar..."
+
+console.log("Rafael: " + voto1)
+console.log("Luisa: " + voto2)
